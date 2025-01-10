af42511268cad11ed036eb65a36c4c013333fad0
Sanchir01 <emgushovs@mail.ru>
2025-01-10 11:17:50 +0300

    update port app to 4200

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
+	devServer: {
+		port: 4200
+	},
 	app: {
 		head: {
 			script: [{ src: 'https://telegram.org/js/telegram-web-app.js' }]
