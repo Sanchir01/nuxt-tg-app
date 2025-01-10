720cebddf6e8098acc150102643814a4e0abbea9
Sanchir01 <emgushovs@mail.ru>
2025-01-10 10:02:44 +0300

    add test tg

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 
+const { MainButton } = await import("vue-tg")
+
 
 </script>
 
 
 <template>
-    <NuxtLayout name="defaults">main page</NuxtLayout>
+  <MainButton text="Главная кнопка" color="rgba(233,130,120,0.2)" @click="() => console.log("hello world ") "/>
+  sadasd
 </template>
---
+++ src/shared/layouts/default.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+    console.log("sanchir")
+</script>
+<template>
+<section>
+    <main>
+      <ClientOnly>
+        <slot/>
+      </ClientOnly>
+    </main>
+</section>
+</template>
