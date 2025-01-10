e0aa8b9e491b959a4122b337d6d9f65484ac32f8
Sanchir01 <emgushovs@mail.ru>
2025-01-10 10:08:05 +0300

    fix error build

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 
-const { MainButton } = await import("vue-tg")
+const { MainButton,useWebAppPopup } = await import("vue-tg")
 
+
+const { showAlert } = useWebAppPopup()
 
 </script>
 
 
 <template>
-  <MainButton text="Главная кнопка" color="rgba(233,130,120,0.2)" @click="() => console.log("hello world ") "/>
+  <MainButton text="Главная кнопка" color="rgba(233,130,120,0.2)"  @click="() => showAlert('Hello!')" />
   sadasd
 </template>
